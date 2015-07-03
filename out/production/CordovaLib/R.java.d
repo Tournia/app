80370eba3aead2d9ece568d341ff591d06b15880
Sjoerd Smink <sjoerdsmink@gmail.com>
2015-07-03 13:39:45 +0200

    Release iOS and Android app

---
+++ out/production/CordovaLib/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/sjoerdsmink/Desktop/app/platforms/android/CordovaLib/ant-build/AndroidManifest.xml \
---
+++ out/production/android/R.java.d
@@ -0,0 +1,22 @@
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-gen/net/tournia/app/R.java \
+ : /Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable/icon.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable-land-hdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable-land-ldpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable-land-mdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable-land-xhdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable-port-hdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable-port-ldpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable-port-mdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/drawable-port-xhdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/values/strings.xml \
+/Users/sjoerdsmink/Desktop/app/platforms/android/res/xml/config.xml \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable/icon.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable-land-hdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable-land-ldpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable-land-mdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable-land-xhdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable-port-hdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable-port-ldpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable-port-mdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/res/drawable-port-xhdpi/screen.png \
+/Users/sjoerdsmink/Desktop/app/platforms/android/ant-build/AndroidManifest.xml \
