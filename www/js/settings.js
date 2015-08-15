1360ca01d37494dc313ef3a313b0fd36da3faedb
Sjoerd Smink <sjoerdsmink@gmail.com>
2015-08-15 09:06:55 +0200

    Update default settings

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,10 +1,9 @@
 // Settings.js
-var apiUrl = 'http://192.168.50.4/api/v2';
-var oAuthClientId = '1_2937prh18fb4s8kk88sg4gwc4g4ogosswwo8k0ow4sgsssk44o';
-var oAuthClientSecret = 'xv3bbprmxqock8c8o4ck4gg8sgkc884s0ks0koow4w4oskg4k';
+var apiUrl = 'http://192.168.50.4/app.php/api/v2';
+var oAuthClientId = '1_124d2062llwggc4socoo4g4gogsgg0kok4swkwokssgk8wok0s';
+var oAuthClientSecret = '3jjjqtrjnv8kogsckccksk08s4k4c0osskkock8c8k0owoc0w0';
 
 // Enabling webMode removes the hashtag from the url and disables functionality which requires logging in
 // Also set in index.html the base and remove cordova.js: <base href="..."> (e.g. to "/live/index.html") and remove <script src="cordova.js"></script>
 var webMode = false;
-
-var desktopUrl = 'http://www.tournia.net/';
+var desktopUrl = 'http://192.168.50.4/';
